8b01ef6335457e3d37bbc2e8fb31d0e5fe235225
Oluwasayomi <ayandearie@yahoo.com>
2025-08-26 01:38:14 +0100

    base settings done
    Base settings for tailwind and react setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+    <h2>Functional Arrow Components</h2>
+    </div>
+  )
+} 
+
+export default App
