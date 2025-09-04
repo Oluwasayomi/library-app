39da02e7ed065b90536424f987c9ff934904f777
Oluwasayomi <ayandearie@yahoo.com>
2025-09-04 19:25:06 +0100

    Hero banner
    Hero banner image and title done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.png'
+import hero_title from '../../assets/hero_title.png'
 
 const Home = () => {
   return (
     <div className='home'>
-    <Navbar/>
+      <Navbar/>
+      <div className='hero'>
+        <img src={hero_banner} alt="banner image" className='banner-img'/>
+        <div className='hero-caption'>
+          <img src={hero_title} alt="banner title" className='caption-img'/>
+          <p>After her death, she goes around worlds solving missions just for... fun?</p>
+        </div>
+      </div>
     </div>
   )
 }
