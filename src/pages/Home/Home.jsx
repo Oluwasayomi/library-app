c69b0f20634f7441b9bc8856857c904a0718b4fc
Oluwasayomi <ayandearie@yahoo.com>
2025-09-05 21:53:07 +0100

    base title cards done
    Base title card created for popular books

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.png'
 import hero_title from '../../assets/hero_title2.png'
+import read_icon from '../../assets/read_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
 
 const Home = () => {
   return (
@@ -13,7 +16,19 @@ const Home = () => {
         <div className='hero-caption'>
           <img src={hero_title} alt="banner title" className='caption-img'/>
           <p>After her death, she goes around worlds solving missions just for... fun?</p>
+          <div className='hero-btns'>
+            <button className='btn'><img src={read_icon} alt="read"/>Read</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="info"/>More Info</button>
+          </div>
+          <TitleCards/>
         </div>
+      </div>
+      
+      <div>
+        <TitleCards/>
+        <TitleCards/>
+        <TitleCards/>
+        <TitleCards/>
       </div>
     </div>
   )
