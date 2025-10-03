c1b489ab5277e9cbb8fefff5803355b682590b28
Oluwasayomi <ayandearie@yahoo.com>
2025-10-03 21:22:18 +0100

    Cards and Book id added
    Cards id added as well as personalised items for each book in book details

--- src/assets/cards/Cards_data.js
+++ src/assets/cards/Cards_data.js
@@ -15,71 +15,85 @@ import transmigration from './transmigration.png';
 
 const cards_data = [
     {
+        id: 1,
         image:agent,
         name:"Secret Agent Sharon",
         categories: ["Upcoming", "Mystery"]
     },
     {
+        id: 2,
         image:detectives,
         name:"Detectives and Secrets",
         categories: ["Mystery", "Popular", "Recommended"]
     },
     {
+        id: 3,
         image:diary,
         name:"Diary of Tags",
         categories: ["Recommended", "Popular"]
     },
     {
+        id: 4,
         image:dreams,
         name:"Dreams of the Fake Princess?",
         categories: ["Upcoming", "Fantasy"]
     },
     {
+        id: 5,
         image:letter,
         name:"Letter About My Love",
         categories: ["Fantasy", "Recommended"]
     },
     {
+        id: 6,
         image:lost,
         name:"Lost Memories",
         categories: ["Upcoming", "Fantasy", "Mystery", "Recommended"]
     },
     {
+        id: 7,
         image:mini,
         name:"My Mini Stories",
         categories: ["Fantasy", "Mystery", "Popular", "Recommended"]
     },
     {
+        id: 8,
         image:pictures,
         name:"Anime Pictures",
         categories: ["Recommended", "Popular"]
     },
     {
+        id: 9,
         image:read,
         name:"Transmigration: I Just Want To Read",
         categories: ["Upcoming", "Fantasy", "Recommended"]
     },
     {
+        id: 10,
         image:reincarnation,
         name:"Reincarnation: Just What Do You Want Me To Do?",
         categories: ["Fantasy", "Popular", "Recommended"]
     },
     {
+        id: 11,
         image:royal,
         name:"Royal Hunt",
         categories: ["Fantasy", "Mystery", "Recommended", "Popular"]
     },
     {
+        id: 12,
         image:secret1,
         name:"Her Secret Identity: Mystery Guy",
         categories: ["Mystery", "Popular", "Recommended"]
     },
     {
+        id: 13,
         image:secret2,
         name:"Her Secret Identity: Missing Brother",
         categories: ["Upcoming", "Mystery"]
     },
     {
+        id: 14,
         image:transmigration,
         name:"Transmigration of Starfire: Let Have Some Fun~",
         categories: ["Popular", "Fantasy", "Recommended"]
--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import './Book.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import cards_data from '../../assets/cards/Cards_data'
 
 const Book = () => {
+  const { id } = useParams();
+  const book = cards_data.find(cards => cards.id === Number(id));
+
+  if (!book) {
+    return <div className='book'><p>Book not found.</p></div>;
+  }
+
   return (
     <div className='book'>
       <img src={back_arrow_icon} alt="back arrow" />
@@ -11,9 +20,9 @@ const Book = () => {
       title='wattpad' frameBorder='0' allowFullScreen></iframe>
 
       <div className='book-info'>
-        <p>Published Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>Published On: Wattpad</p>
+        <p>Name: {book.name}</p>
+        <p>Type: {book.categories.join(', ')}</p>
       </div>
     </div>
   )
